71fed147b9f7710b6082ca47cf626f4a69630eb2
crlttdnt <carlotta.donato03@gmail.com>
2024-12-20 12:13:32 +0100

    Fix delivery time display to handle undefined menu

--- view/screens/OrderScreen.js
+++ view/screens/OrderScreen.js
@@ -85,7 +85,7 @@ const OrderStatus = ({ orderData, menu }) => {
     <View style={globalStyle.container}>
       <Text>Your order will arrive in: </Text>
       <Text style={globalStyle.deliveryTime}>
-        Consegna: {menu.deliveryTime} min
+        Consegna: {menu?.deliveryTime} min
       </Text>
       <Text>MAPPA</Text>
       <MenuCardPreview menu={menu} />
